d7580fad12b4340747e66204bc27a4af98e08a11
MoltenWolfCub <oliver@tomlins.org.uk>
2025-01-03 21:28:23 +0000

    refactored out drawing bevelled rects
    same code to create the rectangles with the curved corners; so extracted
    into a separate static method

--- client/pages/button_play.py
+++ client/pages/button_play.py
@@ -1,7 +1,7 @@
 from typing import Callable
 import pygame
 from client.settings import Settings
-from common.data_types import Vec2D
+from common.data_types import Color, Vec2D
 
 
 class PlayButton:
@@ -18,40 +18,31 @@ class PlayButton:
 
         # outline
         outline_color = Settings.color_menu_button_outline_alt if self.alternate_color else Settings.color_menu_button_outline
-
-        vertical_outline_rect: pygame.Rect = scaler(pygame.Rect(self.rect.x+28, self.rect.y, self.rect.w-56, self.rect.h))
-        pygame.draw.rect(screen, outline_color.to_tuple(), vertical_outline_rect)
-
-        horizontal_outline_rect: pygame.Rect = scaler(pygame.Rect(self.rect.x, self.rect.y+28, self.rect.w, self.rect.h-56))
-        pygame.draw.rect(screen, outline_color.to_tuple(), horizontal_outline_rect)
-
-        outline_curve_tl: pygame.Rect = scaler(pygame.Rect(self.rect.x+28, self.rect.y+28, 28, 28))
-        outline_curve_tr: pygame.Rect = scaler(pygame.Rect(self.rect.x+self.rect.w-28, self.rect.y+28, 28, 28))
-        outline_curve_bl: pygame.Rect = scaler(pygame.Rect(self.rect.x+28, self.rect.y+self.rect.h-28, 28, 28))
-        outline_curve_br: pygame.Rect = scaler(pygame.Rect(self.rect.x+self.rect.w-28, self.rect.y+self.rect.h-28, 28, 28))
-
-        pygame.draw.circle(screen, outline_color.to_tuple(), outline_curve_tl.topleft, outline_curve_tl.w)
-        pygame.draw.circle(screen, outline_color.to_tuple(), outline_curve_tr.topleft, outline_curve_tr.w)
-        pygame.draw.circle(screen, outline_color.to_tuple(), outline_curve_bl.topleft, outline_curve_bl.w)
-        pygame.draw.circle(screen, outline_color.to_tuple(), outline_curve_br.topleft, outline_curve_br.w)
+        self._draw_bevelled_rect(screen, scaler, self.rect, 28, outline_color)
 
         # block
-        vertical_block_rect: pygame.Rect = scaler(pygame.Rect(self.rect.x+10+20, self.rect.y+10, self.rect.w-20-40, self.rect.h-20))
-        pygame.draw.rect(screen, Settings.color_menu_button.to_tuple(), vertical_block_rect)
 
-        horizontal_block_rect: pygame.Rect = scaler(pygame.Rect(self.rect.x+10, self.rect.y+10+20, self.rect.w-20, self.rect.h-20-40))
-        pygame.draw.rect(screen, Settings.color_menu_button.to_tuple(), horizontal_block_rect)
-
-        block_curve_tl: pygame.Rect = scaler(pygame.Rect(self.rect.x+10+20, self.rect.y+10+20, 20, 20))
-        block_curve_tr: pygame.Rect = scaler(pygame.Rect(self.rect.x+10+self.rect.w-40, self.rect.y+10+20, 20, 20))
-        block_curve_bl: pygame.Rect = scaler(pygame.Rect(self.rect.x+10+20, self.rect.y+10+self.rect.h-40, 20, 20))
-        block_curve_br: pygame.Rect = scaler(pygame.Rect(self.rect.x+10+self.rect.w-40, self.rect.y+10+self.rect.h-40, 20, 20))
-
-        pygame.draw.circle(screen, Settings.color_menu_button.to_tuple(), block_curve_tl.topleft, block_curve_tl.w)
-        pygame.draw.circle(screen, Settings.color_menu_button.to_tuple(), block_curve_tr.topleft, block_curve_tr.w)
-        pygame.draw.circle(screen, Settings.color_menu_button.to_tuple(), block_curve_bl.topleft, block_curve_bl.w)
-        pygame.draw.circle(screen, Settings.color_menu_button.to_tuple(), block_curve_br.topleft, block_curve_br.w)
+        block_rect: pygame.Rect = pygame.Rect(self.rect.x+10, self.rect.y+10, self.rect.w-20, self.rect.h-20)
+        self._draw_bevelled_rect(screen, scaler, block_rect, 20, Settings.color_menu_button)
 
         # border
 
         # text
+
+    @staticmethod
+    def _draw_bevelled_rect(screen: pygame.Surface, scaler: Callable[[pygame.Rect], pygame.Rect], rect: pygame.Rect, radius: int, color: Color) -> None:
+        vertical: pygame.Rect = scaler(pygame.Rect(rect.x+radius, rect.y, rect.w - radius*2, rect.h))
+        pygame.draw.rect(screen, color.to_tuple(), vertical)
+
+        horizontal: pygame.Rect = scaler(pygame.Rect(rect.x, rect.y+radius, rect.w, rect.h - radius*2))
+        pygame.draw.rect(screen, color.to_tuple(), horizontal)
+
+        curve_tl: pygame.Rect = scaler(pygame.Rect(rect.x+radius, rect.y+radius, radius, radius))
+        curve_tr: pygame.Rect = scaler(pygame.Rect(rect.x+rect.w-radius, rect.y+radius, radius, radius))
+        curve_bl: pygame.Rect = scaler(pygame.Rect(rect.x+radius, rect.y+rect.h-radius, radius, radius))
+        curve_br: pygame.Rect = scaler(pygame.Rect(rect.x+rect.w-radius, rect.y+rect.h-radius, radius, radius))
+
+        pygame.draw.circle(screen, color.to_tuple(), curve_tl.topleft, curve_tl.w)
+        pygame.draw.circle(screen, color.to_tuple(), curve_tr.topleft, curve_tr.w)
+        pygame.draw.circle(screen, color.to_tuple(), curve_bl.topleft, curve_bl.w)
+        pygame.draw.circle(screen, color.to_tuple(), curve_br.topleft, curve_br.w)
