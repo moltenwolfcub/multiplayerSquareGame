2cd92fc9c042d76b6befc41cf45b3d3d475351c5
MoltenWolfCub <oliver@tomlins.org.uk>
2025-01-04 11:03:19 +0000

    added drop shadow to play button

--- client/pages/button_play.py
+++ client/pages/button_play.py
@@ -6,6 +6,41 @@ from client.assets import fonts
 from client.settings import Settings
 from common.data_types import Color, Vec2D
 
+try:
+    from PIL import Image, ImageFilter
+
+    def drop_shadow(size: Vec2D, blur_radius: int = 50) -> pygame.Surface:
+        img_size: tuple[int, int] = (size.x*4 + blur_radius*2, size.y*4 + blur_radius*2)
+
+        # raw mask
+        mask = pygame.Surface(img_size)
+        mask.fill((0,0,0))
+        pygame.draw.rect(mask, (255,255,255), pygame.Rect((img_size[0]-size.x)/2, (img_size[1]-size.y)/2, size.x, size.y)) #rect in center of img_size
+
+        # blur mask
+        pillow_mask: Image.Image = Image.frombytes("RGB", img_size, pygame.image.tobytes(mask, "RGB", False))
+        blurred_mask = pillow_mask.filter(ImageFilter.GaussianBlur(radius=blur_radius))
+
+        # grascale to alpha
+        alpha_mask = blurred_mask.convert('L')
+
+        shadow: Image.Image = Image.new("RGBA", img_size, (0, 0, 0)) # shadow color
+        shadow.putalpha(alpha_mask)
+
+        pygame_shadow = pygame.image.frombytes(shadow.tobytes(), img_size, "RGBA", False)
+
+        return pygame_shadow
+
+except ModuleNotFoundError:
+    def drop_shadow(size: Vec2D, blur_radius: int = 50) -> pygame.Surface:
+        new_rect: pygame.Rect = pygame.Rect(0, 0, size.x+4, size.y+4)
+
+        shadow = pygame.Surface(new_rect.size, pygame.SRCALPHA)
+        shadow.fill((0,0,0))
+        shadow.set_alpha(80)
+
+        return shadow
+
 
 class PlayButton:
 
@@ -18,6 +53,12 @@ class PlayButton:
     def draw(self, screen: pygame.Surface, scaler: Callable[[pygame.Rect], pygame.Rect]) -> None:
 
         # drop shadow
+
+        shadow: pygame.Surface = drop_shadow(Vec2D(*scaler(self.rect).size))
+        shadow_rect = shadow.get_rect()
+        shadow_rect.center = scaler(self.rect).center
+
+        screen.blit(shadow, shadow_rect)
 
         # border
         outline_color = Settings.color_menu_button_outline_alt if self.alternate_color else Settings.color_menu_button_outline
